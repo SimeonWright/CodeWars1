23ad9b2f1feef2cea47512b5c4187840eb4dce3c
Simeon Wright <simeonwright@Simeons-MacBook-Air.local>
2022-10-17 23:46:56 -0400

    Simple Multplication CodeWar

---
+++ SimpleMult.js
@@ -0,0 +1,10 @@
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+unction simpleMultiplication(number) {
+    if(number % 2 == 0) {
+   return number * 8
+ }else {
+   return number * 9
+ }
+ 
+}
