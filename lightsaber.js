0fd18b00ccb31d11dccf588572c08dbf3cc867a0
Simeon Wright <simeonwright@Simeons-MacBook-Air.local>
2022-11-14 23:30:06 -0500

    lightsaber codewar

---
+++ lightsaber.js
@@ -0,0 +1,6 @@
+// Inspired by the development team at Vooza, write the function that accepts the name of a programmer, and returns the number of lightsabers owned by that person.
+
+
+function howManyLightsabersDoYouOwn(name = "") {
+    return name === 'Zach' ? 18 : 0
+}
