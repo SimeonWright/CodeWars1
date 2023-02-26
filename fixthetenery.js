d64305f19c2b225363259da8e76a4c0f91e3882b
Simeon Wright <simeonwright@Simeons-MacBook-Air.local>
2023-02-25 22:07:57 -0500

    Fix the tenery

---
+++ fixthetenery.js
@@ -0,0 +1,9 @@
+// Your task is to shorten the code as much as possible. Note that submitting the given code will not work because there is a character limit of 137.
+
+// I'll give you a few hints:
+
+
+function describeAge(age) {
+  return "You're a(n) " + (age < 13 ? "kid" : age < 18 ? "teenager" : age < 65 ? "adult" : "elderly")
+}
+
