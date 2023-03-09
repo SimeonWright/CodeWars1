bff4d7d9766e4b1841b9d627e14acf53f626dd59
Simeon Wright <simeonwright@Simeons-MacBook-Air.local>
2023-03-08 22:51:54 -0500

    longest prefix

---
+++ prefixOf.js
@@ -0,0 +1,16 @@
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+
+let prefix = strs[0];
+
+for (let i = 1; i < strs.length; i++) {
+  while (strs[i].indexOf(prefix) !== 0) {
+    prefix = prefix.slice(0, prefix.length - 1);
+    if (prefix === "") {
+      return "";
+    }
+  }
+}
+
+return prefix;
+}
